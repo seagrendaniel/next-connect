56a5647d8bce44b7a44fa58f455257fbc5197896
Dan Seagren <seagrendaniel562@gmail.com>
2020-05-13 15:04:04 -0700

    add toggleLikes controller function

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -53,8 +53,29 @@ exports.getPostsByUser = async (req, res) => {
   return res.json(posts);
 };
 
-exports.getPostFeed = () => {};
+exports.getPostFeed = async (req, res) => {
+  const {following, _id} = req.profile;
 
-exports.toggleLike = () => {};
+  following.push(_id);
+  const posts = await Post.find({postedBy: {$in: following}}).sort({
+    createdAt: "desc"
+  })
+  res.json(posts);
+};
+
+exports.toggleLike = async (req, res) => {
+  const {postId} = req.body;
+
+  const post = await Post.findOne({_id: postId});
+  const likeIds = post.likes.map(id => {id.toString()});
+  const authUserId = req.user._id.toString();
+  if(likeIds.includes(authUserId)) {
+    await post.likes.pull(authUserId);
+  } else {
+    await post.likes.push(authUserId);
+  }
+  await post.save();
+  res.json(post);
+};
 
 exports.toggleComment = () => {};
