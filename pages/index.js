3cd0b4de77f672435e5c75d9b37862a63e5cf5e0
Dan Seagren <seagrendaniel562@gmail.com>
2020-05-19 20:20:09 -0700

    add unAuth user splash page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,43 @@
-// import CircularProgress from "@material-ui/core/CircularProgress";
-// import Drawer from "@material-ui/core/Drawer";
-// import Typography from "@material-ui/core/Typography";
-// import Grid from "@material-ui/core/Grid";
-// import Button from "@material-ui/core/Button";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { authInitialProps } from "../lib/auth";
-
-class Index extends React.Component {
-  state = {};
-
-  render() {
-    return <div>Index</div>;
-  }
+import Router from 'next/router';
+const Index = ({classes, auth}) => {
+  return (
+    <main className={classes.root}>
+{auth.user && auth.user._id ? (
+  //Auth User Page
+  <div>Auth User Page</div>
+) : (
+  <Grid
+    justify="center"
+    alignItems="center"
+    direction="row"
+    container
+    className={classes.heroContent}
+  >
+    <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+      A Better Social Network
+    </Typography>
+    <Typography variant="h6" align="center" color="textSecondary" component="p">
+      Aute anim voluptate cupidatat aute. In fugiat magna aliqua tempor aliqua laborum aliqua ea laboris nisi reprehenderit. Ex nisi velit esse Lorem sunt proident. Adipisicing magna fugiat dolor veniam Lorem aliquip pariatur commodo ex cupidatat nisi ipsum dolore. Veniam consectetur velit eiusmod qui adipisicing cupidatat. Duis adipisicing dolor officia excepteur dolore pariatur ex eu aliqua.
+    </Typography>
+    <Button
+      className={classes.fabButton}
+      variant="extendedFab"
+      color="primary"
+      onClick={() => Router.push('/signup')}
+    >
+      Get Started
+    </Button>
+  </Grid>
+)}
+</main>
+  )
 }
 
 const styles = theme => ({
